cad8ba2197039f807c6499cc6e078264fb4bda9f
Alastair McCormack <alastair@alumedia.co.uk>
2022-01-02 20:08:13 +0000

    FEAT: Pypi metadata

--- setup.py
+++ setup.py
@@ -2,8 +2,28 @@ from setuptools import setup, find_packages
 
 version = '0.1.0'
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='ecs-tunnel',
+    author="Alastair McCormack",
+    author_email="alastair@alumedia.co.uk",
+    description="Tunnel ports via AWS ECS Tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alastairmccormack/ecs-tunnel",
+    project_urls={
+        "Bug Tracker": "https://github.com/alastairmccormack/ecs-tunnel/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+        "Topic :: System :: Systems Administration",
+        "Topic :: System :: Networking"
+    ],
     version=version,
     packages=find_packages(),
     include_package_data=True,
